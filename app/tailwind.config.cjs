bb53e682974381cc7f1d18b4337e31cca87cc49a
J-Josu <josue.sa.trabajos@gmail.com>
2024-01-27 15:57:03 -0300

    Added daisyui

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,15 +1,34 @@
 const typography = require("@tailwindcss/typography");
 const forms = require("@tailwindcss/forms");
+const daisyui = require("daisyui")
+
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    extend: {},
-  },
+    theme: {
+        extend: {},
+    },
 
-  plugins: [forms, typography],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    "primary": "#ffffff",
+                    "secondary": "#ffffff",
+                    "accent": "#ffffff",
+                    "neutral": "#f3f4f6",
+                    "base-100": "#f3f4f6",
+                    "info": "#38bdf8",
+                    "success": "#4ade80",
+                    "warning": "#facc15",
+                    "error": "#f87171",
+                },
+            },
+        ],
+    },
+    plugins: [forms, typography, daisyui],
 };
 
 module.exports = config;
