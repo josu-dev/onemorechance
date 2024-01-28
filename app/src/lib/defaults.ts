7c55df242b22e2869c4c0a8295212ee0bb28dbab
J-Josu <josue.sa.trabajos@gmail.com>
2024-01-28 12:37:18 -0300

    Fixed user syncronization on room

--- app/src/lib/defaults.ts
+++ app/src/lib/defaults.ts
@@ -1,6 +1,8 @@
 export const GAME = {
     ROUNDS: 10,
     COMPLETE_ROUND_TIME: 20 * 1000,
+    ROUND_CHOOSE_TIME: 20 * 1000,
     OPTIONS: 10,
     ROUND_RESULTS_TIME: 10 * 1000,
+    ROUND_RATE_TIME: 15 * 1000,
 } as const;
