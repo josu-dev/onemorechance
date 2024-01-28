1fd7d29174b5d3eb2df887adf1526a16b050522d
J-Josu <josue.sa.trabajos@gmail.com>
2024-01-28 11:02:58 -0300

    Improved game state handling

--- app/src/types.ts
+++ app/src/types.ts
@@ -1,4 +1,4 @@
-import type { GameStatus } from './lib/enums.js';
+import type { GameStatus, RoomStatus } from './lib/enums.js';
 
 export type User = {
     id: string,
@@ -13,15 +13,17 @@ export type Modifier = {
 export type PlayerRole = 'HOST' | 'INVITED';
 
 export type Player = {
-    role: PlayerRole,
     userId: string,
+    name: string,
+    role: PlayerRole,
     score: number,
     ready: boolean,
-    modifiers: Modifier[],
     phrases: Phrase[],
-    selectedOption?: Option,
     options: Option[],
-}
+    selectedOption?: Option,
+    freestyle?: string[],
+    modifiers: Modifier[],
+};
 
 export type Phrase = {
     id: string,
@@ -43,7 +45,7 @@ export type DeckIdentifier = {
 export type Deck = {
     id: string,
     name: string,
-    cards: Phrase[],
+    phrases: Phrase[],
     options: Option[],
 };
 
@@ -52,27 +54,23 @@ export type Game = {
     status: GameStatus;
     maxRounds: number;
     maxOptions: number;
-    roundTime: number;
+    chooseTime: number;
     round: number,
     deck: DeckIdentifier,
     phrase: Phrase,
     usedPhrases: string[],
     usedOptions: string[],
     players: Player[],
+    lastWinner?: string,
 };
 
 export type Room = {
     id: string,
+    status: RoomStatus,
     host: User,
     users: User[],
     readyCount: number,
     game: Game,
-};
-
-export type Message = {
-    author: string;
-    text: string;
-    bot: boolean;
 };
 
 
@@ -85,7 +83,6 @@ export type ServerToClientEvents = {
     user_joined_room: (data: User) => void;
     user_left_room: (data: User) => void;
     game_started: (data: Game) => void;
-    room_message: (data: Message) => void;
     availible_decks_update: (data: DeckIdentifier[]) => void;
     updated_round: (data: Game) => void;
     selection_end: (data: Game) => void;
@@ -96,14 +93,14 @@ export type ClientToServerEvents = {
     register_user: (data: { userId?: string, name: string; }) => void;
     unregister_user: (data: { userId: string; }) => void;
     create_room: (data: { userId: string; }) => void;
-    update_room: (data: { roomId: string; data: Room }) => void;
+    update_room: (data: { roomId: string; data: Room; }) => void;
     join_room: (data: { roomId: string; userId: string; }) => void;
     leave_room: (data: { roomId: string; userId: string; }) => void;
     player_ready: (data: { roomId: string; userId: string; }) => void;
     player_unready: (data: { roomId: string; userId: string; }) => void;
     start_game: (data: { roomId: string; userId: string; }) => void;
     trigger_decks_update: () => void;
-    get_new_round: (data: { roomId: string; userId: string; options: number }) => void;
+    get_new_round: (data: { roomId: string; userId: string; options: number; }) => void;
     option_selected: (data: { roomId: string; userId: string; option: Option; }) => void;
 };
 
