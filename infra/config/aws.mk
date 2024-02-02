4c4306ebd523b4c81b6594262745f9f7dfb93f3a
Jonathan <emeraldusk@gmail.com>
2024-02-02 18:57:57 -0300

    chore: migrate to render

--- infra/config/aws.mk
+++
@@ -1,7 +0,0 @@
-image_id ?= ami-0261755bbcb8c4a84
-instance_type ?= t2.micro
-key_name ?= jonathan@jonathan
-security_group_ids ?= sg-0b156e283023b9fc2
-subnet_id ?= subnet-0bcc53200daecb50e
-vpc_id ?= vpc-074271020d1e20c8f
-name ?= omc
--- infra/terraform/modules/aws/output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_dns" {
-  value       = aws_instance.omc.public_dns
-  description = "Public DNS of the instance"
-}
