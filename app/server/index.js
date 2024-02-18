4da199245bb97fa04c4b81fc21e5e28a6b2acded
J-Josu <josue.sa.trabajos@gmail.com>
2024-02-18 17:23:00 -0300

    Added heath check for express server

--- app/server/index.js
+++ app/server/index.js
@@ -4,12 +4,20 @@ import { attach_socket_server } from "../build/server_ws/server/sockets.js";
 
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+app.get('/healthz', (req, res) => {
+    res
+        .setHeader('Content-Type', 'text/plain')
+        .status(200)
+        .send('OK');
+});
+
+app.use(handler);
 
 const server = app.listen(PORT, () => {
     console.info("server is listening on port", PORT);
 });
-
-app.use(handler);
 
 attach_socket_server(server);
